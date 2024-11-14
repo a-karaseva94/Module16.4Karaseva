a9b9035027d890e0c2fafd1ed4a9655c81ff023e
a-karaseva94 <a-karaseva94@mail.ru>
2024-11-14 17:35:48 +0300

    Add files via upload

---
+++ module_16_4.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Path, status, HTTPException
+from pydantic import BaseModel
+from typing import Annotated, List
+
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int
+
+
+# реализация CRUD запросов
+# get запрос по маршруту '/users'
+@app.get("/users")
+async def get_users() -> List[User]:
+    return users
+
+
+# post запрос по маршруту '/user/{username}/{age}'
+@app.post("/user/{username}/{age}")
+async def create_users(
+        username: Annotated[str, Path(min_length=5, max_length=20,
+                                      description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> User:
+    user_id = str(len(users) + 1)
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+# put запрос по маршруту '/user/{user_id}/{username}/{age}'
+@app.put("/user/{user_id}/{username}/{age}")
+async def update_users(
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")],
+        username: Annotated[str, Path(min_length=5, max_length=20,
+                                      description="Enter username", example="UrbanUser")],
+        age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> User:
+    for user in users:
+        if user.id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
+
+
+# delete запрос по маршруту '/user/{user_id}'
+@app.delete("/user/{user_id}")
+async def delete_user(
+        user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example="1")]):
+    for user in users:
+        if user.id == user_id:
+            users.remove(user)
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
